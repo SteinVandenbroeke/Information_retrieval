1147ef22abcec554ca4441989a55225a8605b7d5
Stein Vandenbroeke <stein.vandenbroeke@student.uantwerpen.be>
2024-10-25 01:01:03 +0200

    down to 6gb

--- InMemoryOf/inverted_indexing.py
+++ InMemoryOf/inverted_indexing.py
@@ -1,10 +1,31 @@
 import os
 import re
+import sys
 from collections import defaultdict
+import datetime
+import numpy as np
 
 def tokenize_documents(path_to_documents):
-    inverted_index = defaultdict(set)
+    tokecounter = defaultdict(lambda: np.array([0, 0]))
+    counter = 0
+    print("start")
+    for d in os.listdir(path_to_documents):
+        if d.endswith('.txt'):
+            file_path = os.path.join(path_to_documents, d)
+            with open(file_path, 'r') as file:
+                terms = re.sub(r'\W+', ' ', file.read().lower()).split()
+                for x in terms:
+                    tokecounter[x][0] += 1
+        if (counter % 10000 == 0):
+            print(counter, " | size: ", sys.getsizeof(tokecounter), " | items", len(tokecounter))
+        counter += 1
+
+    print("token counter speedtest")
+
+    start_time = datetime.datetime.now()
+    inverted_index = defaultdict()
     print(len(os.listdir(path_to_documents)), " documents found.")
+    counter = 0
     for d in os.listdir(path_to_documents):
         if d.endswith('.txt'):
             doc_id = int(d.replace("output_", "").replace(".txt", ""))
@@ -12,5 +33,22 @@ def tokenize_documents(path_to_documents):
             with open(file_path, 'r') as file:
                 terms = re.sub(r'\W+', ' ', file.read().lower()).split()
                 for x in terms:
-                    inverted_index[x].add(doc_id)
+                    if x in inverted_index:
+                        inverted_index[x][tokecounter[x][1]] = doc_id
+                    else:
+                        array = np.empty(tokecounter[x][0])
+                        array[0] = doc_id
+                        inverted_index[x] = array
+                    tokecounter[x][1] += 1
+
+        if(counter % 10000 == 0):
+            print(counter, " | size: ",  sys.getsizeof(inverted_index), " | items", len(inverted_index))
+        counter += 1
+
+    total_size = sys.getsizeof(inverted_index)  # Size of the dictionary itself
+    for key, value_set in inverted_index.items():
+        total_size += sys.getsizeof(value_set)  # Add the size of each set
+    print("total size: ", total_size)
     print("done indexing")
+    end_time = datetime.datetime.now()
+    print("time measurement: ", end_time - start_time)
--- InMemoryOf/main.py
+++ InMemoryOf/main.py
@@ -1,4 +1,4 @@
 from InMemoryOf.inverted_indexing import tokenize_documents
 
 if __name__ == '__main__':
-    tokenize_documents('/home/nielsvandenbroeck/School/IR/Information_retrieval/datasets/full_docs_small')
+    tokenize_documents('./../../datasets/full_docs')
